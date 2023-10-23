1a6b079e671f77e385c5be01dbc9871b7a8e7df1
ESPhoenix <132447600+ESPhoenix@users.noreply.github.com>
2023-10-23 14:11:51 +0100

    Add files via upload

---
+++ config_coreFeatures.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# directory information
+inputDir = "/home/eugene/FeatureGeneration/main_dataset_50"
+outputDir = "/home/eugene/FeatureGeneration/main_dataset_50/features"
+
+# msms exe locations
+msmsDir = "/home/eugene/bin/msms"
+msmsExe = "/home/eugene/bin/msms/msms.x86_64Linux2.2.6.1"
+pdb2xyzrExe = "/home/eugene/bin/msms/pdb_to_xyzr"
+
+# fixed inputs
+aminoAcidTable="/home/eugene/FeatureGeneration/tableAmm.txt"
+
