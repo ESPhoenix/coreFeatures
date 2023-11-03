f0982784e12012304dceb0b581dc0dc734ab4109
ESPhoenix <e.s.phoenix@hotmail.co.uk>
2023-11-03 16:06:11 +0000

    move utils to module, change inputs method

--- config_coreFeatures.py
+++ config_coreFeatures.py
@@ -1,14 +1,17 @@
 import numpy as np
 
-# directory information
-inputDir = "/home/eugene/FeatureGeneration/main_dataset_50"
-outputDir = "/home/eugene/FeatureGeneration/main_dataset_50/features"
+def inputs():
+    # directory information
+    inputDir = "/home/eugene/FeatureGeneration/main_dataset_50"
+    outputDir = "/home/eugene/FeatureGeneration/main_dataset_50/features"
 
-# msms exe locations
-msmsDir = "/home/eugene/bin/msms"
-msmsExe = "/home/eugene/bin/msms/msms.x86_64Linux2.2.6.1"
-pdb2xyzrExe = "/home/eugene/bin/msms/pdb_to_xyzr"
+    # msms exe locations
+    msmsDir = "/home/eugene/bin/msms"
+    msmsExe = "/home/eugene/bin/msms/msms.x86_64Linux2.2.6.1"
+    pdb2xyzrExe = "/home/eugene/bin/msms/pdb_to_xyzr"
 
-# fixed inputs
-aminoAcidTable="/home/eugene/FeatureGeneration/tableAmm.txt"
+    # fixed inputs
+    aminoAcidTable="/home/eugene/FeatureGeneration/tableAmm.txt"
+
+    return inputDir, outputDir, msmsDir, msmsExe, pdb2xyzrExe, aminoAcidTable
 
--- coreFeatures.py
+++ coreFeatures.py
@@ -1,6 +1,7 @@
 ########################################################################################
 import os
 from os import path as p
+import sys
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
@@ -8,76 +9,8 @@ import multiprocessing as mp
 import importlib
 import subprocess
 import argpass
+from custom_utils import pdb2df, area2df
 ########################################################################################
-## basic utilities
-def pdb2df(pdbFile):
-    columns = ['ATOM', 'ATOM_ID', 'ATOM_NAME', 'RES_NAME', 'CHAIN_ID', 'RES_SEQ', 'X', 'Y', 'Z', 'OCCUPANCY', 'TEMP_FACTOR', 'ELEMENT']
-
-    data = []
-    with open(pdbFile, 'r') as pdb_file:
-        for line in pdb_file:
-            if line.startswith('ATOM') or line.startswith('HETATM'):
-                atom_type = line[0:6].strip()
-                atom_id = int(line[6:11].strip())
-                atom_name = line[12:16].strip()
-                res_name = line[17:20].strip()
-                chain_id = line[21:22].strip()
-                if chain_id == '':
-                    chain_id = None
-                res_seq = int(line[22:26].strip())
-                x = float(line[30:38].strip())
-                y = float(line[38:46].strip())
-                z = float(line[46:54].strip())
-                occupancy = float(line[54:60].strip())
-                temp_factor = float(line[60:66].strip())
-                element = line[76:78].strip()
-
-                data.append([atom_type, atom_id, atom_name, res_name, chain_id, res_seq, x, y, z, occupancy, temp_factor, element])
-
-    return pd.DataFrame(data, columns=columns)
-
-def calculateEuclideanDistance(row, point):
-    xDiff = row['X'] - point[0]
-    yDiff = row['Y'] - point[1]
-    zDiff = row['Z'] - point[2]
-    euclidean = np.sqrt(xDiff**2 + yDiff**2 + zDiff**2)
-    
-    return float(euclidean)
-
-def ifnotmkdir(dir):
-    if not p.isdir(dir):
-        os.mkdir(dir)
-    return dir
-
-
-def vert2df(vertFile):
-    x = []
-    y = []
-    z = []
-    with open(vertFile,"r") as file:
-        for line in file:
-            if line.startswith("#"):
-                continue
-            cols = line.split()
-            if len(cols) == 4:
-                continue
-            x.append(cols[0])
-            y.append(cols[1])
-            z.append(cols[2])
-    data = {"X" : x, "Y" : y, "Z" : z}
-    pdbDf = pd.DataFrame(data)
-
-def area2df(areaFile):
-    ses =[]
-    with open(areaFile,"r") as file:
-        for line in file:
-            if "Atom" in line:
-                continue
-            cols = line.split()
-            ses.append(float(cols[1]))
-    data = {"SES":ses}
-    pdbDf = pd.DataFrame(data)
-    return pdbDf
 ########################################################################################
 # get inputs
 def read_inputs():
@@ -89,8 +22,20 @@ def read_inputs():
         print('No config file name provided.')
     configName = p.splitext(configName)[0]
 
-    inputScript = importlib.import_module(configName)
-    globals().update(vars(inputScript))
+    # add config to PYTHONPATH
+    cwd = os.getcwd()
+    configPath = p.join(cwd,configName)
+    sys.path.append(configPath)
+    # import config file and run input function to return variables
+    try:
+        config_module = __import__(configName)
+        inputDir, outputDir, msmsDir, msmsExe, pdb2xyzrExe, aminoAcidTable = config_module.inputs()
+        return inputDir, outputDir, msmsDir, msmsExe, pdb2xyzrExe, aminoAcidTable
+    except ImportError:
+        print(f"Error: Can't to import module '{configName}'. Make sure the input exists!")
+        print("HOPE IS THE FIRST STEP ON THE ROAD TO DISAPPOINTMENT")
+        exit()
+
 ########################################################################################
 
 def initialiseAminoAcidInformation(aminoAcidTable):
@@ -238,7 +183,7 @@ def process_pdbs(pdbList, outDir, aminoAcidNames, aminoAcidProperties,msmsDir):
 def main():
     # load user inputs
     read_inputs()
-    outDir=ifnotmkdir(outputDir)
+    outDir=os.makedirs(outputDir, exist_ok=True)
     # initialise amino acid data
     aminoAcidNames, aminoAcidProperties = initialiseAminoAcidInformation(aminoAcidTable)
     # get list of pdbFiles in pdbDir
